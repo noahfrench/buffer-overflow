64aa5969f5d07f9c43032826ee38b1f7dc6e5107
l337 h4x0r <noahjfrench@gmail.com>
2018-10-29 15:41:33 -0400

    add newline character

--- shellcode.s
+++ shellcode.s
@@ -7,6 +7,8 @@ shellcode:
 	jmp afterString
 string:
 	db "Noah French, your grade on this assignment is an A+"
+newline:
+	db "n"
 afterString:
 	xor rax, rax
 	xor rdi, rdi
@@ -21,6 +23,19 @@ afterString:
 	mov dl, 51
 	syscall
 	xor rax, rax
+	mov al, 0x2a
+	sub al, 0x20
+	mov [rel newline], al
+	xor rax, rax
+	xor rdi, rdi
+	xor rsi, rsi
+	mov al, 1
+	mov edi, eax
+	lea rsi, [rel newline]
+	xor rdx, rdx
+	mov dl, 1
+	syscall
+ 	xor rax, rax
 	mov al, 60
 	xor rdi, rdi
 	xor rsi, rsi
